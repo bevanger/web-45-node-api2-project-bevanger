bd35bc0122c7f07296376f023f037a2065bab795
bevanger <baileyevanger@gmail.com>
2021-08-31 15:01:27 -0600

    wrote out two endpoints for get

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,11 +5,29 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    res.status(200).json({ message: 'returns an array of all post objects' })
+   Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "The posts information could not be retrieved" })
+    })
 });
 
 router.get('/:id', (req, res) => {
-    res.status(200).json({ message: 'returns post with specified id' })
+    Posts.findById(req.params.id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        }else {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })    
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    })
 });
 
 router.get('/:id/comments', (req, res) => {
